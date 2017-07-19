e817d85fa642a15012ce4052e2b681118bcc63f4
Mariusz Sowa <mariuszsowa11@gmail.com>
2017-07-19 12:15:51 +0200

    hero selector

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -57,22 +57,36 @@ export class Hero {
     }
   `],
   template: `
+    <h1>{{title}}</h1>
     <h2>My Heroes</h2>
     <ul class="heroes">
-      <li *ngFor="let hero of heroes">
+      <li *ngFor="let hero of heroes"
+          [class.selected]="hero === selectedHero"
+          (click)="onSelect(hero)">
         <span class="badge">{{hero.id}}</span> {{hero.name}}
       </li>
     </ul>
+    <div *ngIf="selectedHero">
+      <h2>{{selectedHero.name}} details!</h2>
+      <div><label>id: </label>{{selectedHero.id}}</div>
+      <div>
+        <label>name: </label>
+        <input [(ngModel)]="selectedHero.name" placeholder="name"/>
+      </div>
+    </div>
   `
 })
 
 export class AppComponent {
+
   title = 'Tour of Heroes';
-  hero: Hero = {
-    id: 1,
-    name: 'Windstorm'
-  };
+  selectedHero: Hero;
   heroes = HEROES;
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
+
 }
 
 const HEROES: Hero[] = [
