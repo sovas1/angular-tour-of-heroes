e05cbaf9d02581eac2a45136930ba8ec4dd7fc80
Mariusz Sowa <mariuszsowa11@gmail.com>
2017-07-19 14:29:04 +0200

    refactor routing module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,32 +6,14 @@ import {HeroDetailComponent} from "./hero-detail.component";
 import {HeroesComponent} from "./heroes.component";
 import {AppComponent} from "./app.component";
 import {HeroService} from "./hero.service";
-import {RouterModule} from "@angular/router";
 import {DashboardComponent} from "./dashboard.component";
+import {AppRoutingModule} from "./app-routing-module";
 
 @NgModule({
   imports:      [
     BrowserModule,
     FormsModule,
-    RouterModule.forRoot([
-      {
-        path: '',
-        redirectTo: '/dashboard',
-        pathMatch: 'full'
-      },
-      {
-        path: 'dashboard',
-        component: DashboardComponent
-      },
-      {
-        path: 'heroes',
-        component: HeroesComponent
-      },
-      {
-        path: 'detail/:id',
-        component: HeroDetailComponent
-      }
-    ])
+    AppRoutingModule
   ],
   declarations: [
     AppComponent,
