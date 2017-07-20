f0e74a3b44ccd1b72ed2e8f7230ed576bbd2bb01
Mariusz Sowa <mariuszsowa11@gmail.com>
2017-07-20 08:36:03 +0200

    from now, app uses http requests to in memory data service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,11 +8,16 @@ import {AppComponent} from "./components/app/app.component";
 import {HeroService} from "./services/hero.service";
 import {DashboardComponent} from "./components/dashboard/dashboard.component";
 import {AppRoutingModule} from "./app-routing-module";
+import {HttpModule} from "@angular/http";
+import {InMemoryWebApiModule} from "angular-in-memory-web-api";
+import {InMemoryDataService} from "./services/in-memory-data.service";
 
 @NgModule({
   imports:      [
     BrowserModule,
     FormsModule,
+    HttpModule,
+    InMemoryWebApiModule.forRoot(InMemoryDataService),
     AppRoutingModule
   ],
   declarations: [
--- src/app/services/hero.service.ts
+++ src/app/services/hero.service.ts
@@ -1,18 +1,35 @@
 import {Injectable} from "@angular/core";
 
 import {Hero} from "../models/hero";
-import {HEROES} from './mock-heroes';
+import {Http} from "@angular/http";
+
+import 'rxjs/add/operator/toPromise';
 
 @Injectable()
 export class HeroService {
 
+  private heroesUrl = 'api/heroes';
+
+  constructor(private http: Http) { }
+
   getHeroes(): Promise<Hero[]> {
-    return Promise.resolve(HEROES);
+    return this.http.get(this.heroesUrl)
+                    .toPromise()
+                    .then(response => response.json().data as Hero[])
+                    .catch(this.handleError);
   }
 
   getHero(id: number): Promise<Hero> {
-    return this.getHeroes()
-               .then(heroes => heroes.find(hero => hero.id === id));
+    const url = '${this.heroUrl}/${id}';
+    return this.http.get(url)
+                    .toPromise()
+                    .then(response => response.json().data as Hero)
+                    .catch(this.handleError);
+  }
+
+  private handleError(error: any): Promise<any> {
+    console.error('An error occurred', error);
+    return Promise.reject(error.message || error);
   }
 
 }
